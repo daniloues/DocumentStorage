e46ed5febc739b7eb80a37a5965f51beacaacd35
daniloues <qs17002@ues.edu.sv>
2024-05-28 16:58:22 -0600

    Test para Jenkins WebHook 5

--- src/test/java/com/mycompany/documientos/boundary/rest/PrimerParcialIT.java
+++ src/test/java/com/mycompany/documientos/boundary/rest/PrimerParcialIT.java
@@ -350,7 +350,7 @@ public class PrimerParcialIT {
     @Test
     @Order(7)
     public void testValidarResultados() {
-        System.out.println("validarResultadosIT T");
+        System.out.println("validarResultadosIT Sera");
 
         System.out.println("============== ID TIPO ATRIBUTO GENERADO " + ID_TIPO_ATRIBUTO_CREADO);
         Response respuesta = target.path("tipoatributo/{idTipoAtributo}")
